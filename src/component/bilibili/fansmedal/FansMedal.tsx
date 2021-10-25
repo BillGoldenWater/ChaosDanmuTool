67575f97a2d8d4cf1f25b06336bb6bdc97f6f0ed
BiliGoldenWater <2439577268@qq.com>
2021-10-25 23:54:40 +0800

    finish FansMedal

--- src/component/bilibili/fansmedal/FansMedal.tsx
+++ src/component/bilibili/fansmedal/FansMedal.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import style from "./FansMedal.module.css";
 import { MedalInfo } from "../../../utils/command/bilibili/MedalInfo";
+import { rgbI2S } from "../../../utils/FormatConverters";
 
 class Props {
   medalInfo: MedalInfo;
@@ -8,12 +9,37 @@ class Props {
 
 export class FansMedal extends React.Component<Props> {
   render(): JSX.Element {
+    if (
+      !this.props.medalInfo ||
+      !this.props.medalInfo.is_lighted ||
+      this.props.medalInfo.is_lighted == 0
+    )
+      return null;
+
+    const medal_color_border = rgbI2S(this.props.medalInfo.medal_color_border);
+    const medal_color_start = rgbI2S(this.props.medalInfo.medal_color_start);
+    const medal_color_end = rgbI2S(this.props.medalInfo.medal_color_end);
+    const medal_color = rgbI2S(this.props.medalInfo.medal_color);
     return (
       <div className={style.FansMedal}>
-        <div className={style.FansMedal_name}>
+        <div
+          className={style.FansMedal_name}
+          style={{
+            borderColor: medal_color_border,
+            backgroundImage: "linear-gradient(45deg,{0},{1})"
+              .replace("{0}", medal_color_start)
+              .replace("{1}", medal_color_end),
+          }}
+        >
           {this.props.medalInfo.medal_name}
         </div>
-        <div className={style.FansMedal_lvl}>
+        <div
+          className={style.FansMedal_lvl}
+          style={{
+            borderColor: medal_color_border,
+            color: medal_color,
+          }}
+        >
           {this.props.medalInfo.medal_level}
         </div>
       </div>
