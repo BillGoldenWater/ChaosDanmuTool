4129ed0decc2480dfd1ae917e31405f668d48125
BiliGoldenWater <2439577268@qq.com>
2021-10-30 21:55:45 +0800

    add InteractWord to DanmuItem

--- src/window/viewer/page/main/danmurender/danmuitem/DanmuItem.tsx
+++ src/window/viewer/page/main/danmurender/danmuitem/DanmuItem.tsx
@@ -3,6 +3,8 @@ import { DanmuMessage } from "../../../../../../utils/command/DanmuMessage";
 import { DanmuMsg } from "./item/danmumsg/DanmuMsg";
 import { parseDanmuMsg } from "../../../../../../utils/command/bilibili/DanmuMsg";
 import { SuperChatMessage } from "./item/superchatmessage/SuperChatMessage";
+import { InteractWord } from "./item/interactword/InteractWord";
+import { InteractWord as TInteractWord } from "../../../../../../utils/command/bilibili/InteractWord";
 
 class Props {
   message: DanmuMessage;
@@ -12,6 +14,9 @@ export class DanmuItem extends React.Component<Props> {
   render(): JSX.Element {
     const msg: DanmuMessage = this.props.message;
     switch (msg.cmd) {
+      case "INTERACT_WORD": {
+        return <InteractWord data={msg as unknown as TInteractWord} />;
+      }
       case "DANMU_MSG": {
         return <DanmuMsg data={parseDanmuMsg(msg)} />;
       }
