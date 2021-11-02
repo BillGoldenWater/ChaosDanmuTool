5a115be8cb5ab3eebaec7e98e65eb0e1cf5d6272
BiliGoldenWater <2439577268@qq.com>
2021-11-02 19:47:22 +0800

    extract ConfigFileManager to single file

--- src/window/main/page/setting/Setting.tsx
+++ src/window/main/page/setting/Setting.tsx
@@ -1,57 +1,15 @@
 import React from "react";
 import style from "./Setting.module.css";
-import { FunctionCard } from "../../../../component/functioncard/FunctionCard";
-import { Button } from "../../../../component/button/Button";
-import { getConfigUpdateMessage } from "../../../../utils/command/ConfigUpdate";
-import { ConfigContext } from "../../utils/ConfigContext";
-import { Config } from "../../../../utils/config/Config";
+import { ConfigFileManager } from "./configfilemanager/ConfigFileManager";
 
 class Props {}
 
 export class Setting extends React.Component<Props> {
-  broadcastConfig(config: Config): void {
-    window.electron.websocketBroadcast(getConfigUpdateMessage(config));
-  }
-
   render(): JSX.Element {
     return (
-      <ConfigContext.Consumer>
-        {({ config, setConfig }) => {
-          return (
-            <div className={style.setting}>
-              <FunctionCard name={"配置文件管理"}>
-                <Button
-                  onClick={() => {
-                    window.electron.loadConfig();
-                    const config: Config = JSON.parse(
-                      window.electron.getConfig()
-                    );
-                    setConfig(config);
-                    this.broadcastConfig(config);
-                  }}
-                >
-                  读取
-                </Button>
-                <Button
-                  onClick={() => {
-                    window.electron.updateConfig(JSON.stringify(config));
-                    window.electron.saveConfig();
-                  }}
-                >
-                  保存
-                </Button>
-                <Button
-                  onClick={() => {
-                    this.broadcastConfig(config);
-                  }}
-                >
-                  更新
-                </Button>
-              </FunctionCard>
-            </div>
-          );
-        }}
-      </ConfigContext.Consumer>
+      <div className={style.setting}>
+        <ConfigFileManager />
+      </div>
     );
   }
 }
---
+++ src/window/main/page/setting/configfilemanager/ConfigFileManager.tsx
@@ -0,0 +1,48 @@
+import React, { ReactNode } from "react";
+import { FunctionCard } from "../../../../../component/functioncard/FunctionCard";
+import { Button } from "../../../../../component/button/Button";
+import { Config } from "../../../../../utils/config/Config";
+import { ConfigContext } from "../../../utils/ConfigContext";
+import { getConfigUpdateMessage } from "../../../../../utils/command/ConfigUpdate";
+
+export class ConfigFileManager extends React.Component {
+  broadcastConfig(config: Config): void {
+    window.electron.websocketBroadcast(getConfigUpdateMessage(config));
+  }
+
+  render(): ReactNode {
+    return (
+      <ConfigContext.Consumer>
+        {({ config, setConfig }) => (
+          <FunctionCard name={"配置文件管理"}>
+            <Button
+              onClick={() => {
+                window.electron.loadConfig();
+                const config: Config = JSON.parse(window.electron.getConfig());
+                setConfig(config);
+                this.broadcastConfig(config);
+              }}
+            >
+              读取
+            </Button>
+            <Button
+              onClick={() => {
+                window.electron.updateConfig(JSON.stringify(config));
+                window.electron.saveConfig();
+              }}
+            >
+              保存
+            </Button>
+            <Button
+              onClick={() => {
+                this.broadcastConfig(config);
+              }}
+            >
+              更新
+            </Button>
+          </FunctionCard>
+        )}
+      </ConfigContext.Consumer>
+    );
+  }
+}
