b40e8335d7826941d9c0e378fdc1fb732d463b9e
BiliGoldenWater <2439577268@qq.com>
2021-11-05 00:20:03 +0800

    add "show/createMainWindow when click dock icon"

--- src/index.tsx
+++ src/index.tsx
@@ -224,7 +224,13 @@ app.on("window-all-closed", () => {
 app.on("activate", () => {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) {
+  if(mainWindow){
+    if (mainWindow.isDestroyed()){
+      createMainWindow();
+    } else {
+      mainWindow.show();
+    }
+  } else {
     createMainWindow();
   }
 });
