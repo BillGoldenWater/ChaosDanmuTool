ac9abb318bbe76fed04047671ba214aede1737ac
BiliGoldenWater <2439577268@qq.com>
2021-11-21 08:24:40 +0800

    add more description to DanmuViewerControl add "view danmu in other app" to DanmuViewerControl add settings to DanmuViewerControl

--- src/window/main/page/danmuviewercontrol/DanmuViewerControl.tsx
+++ src/window/main/page/danmuviewercontrol/DanmuViewerControl.tsx
@@ -1,30 +1,117 @@
 import React, { ReactNode } from "react";
-import { Button, Divider, Space } from "antd";
+import { Button, Divider, Form, Select, Space, Typography } from "antd";
 import { DanmuViewConfigModifier } from "../settings/configmodifier/danmuviewconfigmodifier/DanmuViewConfigModifier";
+import { ConfigContext } from "../../utils/ConfigContext";
 
-export class DanmuViewerControl extends React.Component {
+class Props {}
+
+class State {
+  selectedStyle: string;
+}
+
+export class DanmuViewerControl extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+
+    this.state = {
+      selectedStyle: "internal",
+    };
+  }
+
   render(): ReactNode {
     return (
-      <div>
-        <Space>
-          <Button
-            onClick={() => {
-              window.electron.openViewer();
-            }}
-          >
-            打开
-          </Button>
-          <Button
-            onClick={() => {
-              window.electron.closeViewer();
-            }}
-          >
-            关闭
-          </Button>
-        </Space>
-        <Divider orientation={"left"}>设置</Divider>
-        <DanmuViewConfigModifier />
-      </div>
+      <ConfigContext.Consumer>
+        {({ config }) => {
+          const state = this.state;
+
+          const styleNameList = config.danmuViewCustoms.map((value) => {
+            return value.name;
+          });
+          const styleOptionList = config.danmuViewCustoms.map((value) => {
+            return (
+              <Select.Option key={value.name} value={value.name}>
+                {value.name}
+              </Select.Option>
+            );
+          });
+
+          const verifiedSelectedStyle = styleNameList.includes(
+            state.selectedStyle
+          )
+            ? state.selectedStyle
+            : styleNameList.length > 0
+            ? styleNameList[0]
+            : "";
+
+          return (
+            <div>
+              <Divider orientation={"left"}>在悬浮窗中查看弹幕</Divider>
+
+              <Space>
+                <Typography.Text>悬浮窗开关:</Typography.Text>
+                <Button
+                  onClick={() => {
+                    window.electron.openViewer();
+                  }}
+                >
+                  打开
+                </Button>
+                <Button
+                  onClick={() => {
+                    window.electron.closeViewer();
+                  }}
+                >
+                  关闭
+                </Button>
+              </Space>
+
+              <Divider orientation={"left"}>在其他应用中查看弹幕</Divider>
+
+              <Typography.Paragraph type={"secondary"}>
+                选择你想使用的样式后复制链接至其他应用中
+              </Typography.Paragraph>
+              <Typography.Paragraph type={"secondary"}>
+                OBS: 添加浏览器源后将链接复制至URL栏中,
+                删除自定义CSS中的所有内容
+                <br />
+                修改大小请使用属性内的高度和宽度,
+                不要直接在预览中拖动或使用变换, 否则可能会导致奇怪的效果
+              </Typography.Paragraph>
+              <Typography.Paragraph type={"secondary"}>
+                浏览器: 直接当网页打开即可
+              </Typography.Paragraph>
+              <Typography.Paragraph type={"secondary"}>
+                其他设备: 需要将链接中的 localhost 替换为本机的ip地址,
+                确保防火墙中放行端口 {config.httpServerPort}
+              </Typography.Paragraph>
+
+              <Form.Item label={"要使用的样式"}>
+                <Select
+                  showSearch
+                  value={verifiedSelectedStyle}
+                  onChange={(value) => {
+                    this.setState({ selectedStyle: value });
+                  }}
+                >
+                  {styleOptionList}
+                </Select>
+              </Form.Item>
+
+              <Typography.Paragraph>
+                <Typography.Text>链接: </Typography.Text>
+                <Typography.Text copyable>
+                  {`http://localhost:${config.httpServerPort}/viewer` +
+                    `?maxReconnectAttemptNum=${config.danmuViewConfig.maxReconnectAttemptNumber}` +
+                    `&name=${state.selectedStyle}`}
+                </Typography.Text>
+              </Typography.Paragraph>
+
+              <Divider orientation={"left"}>设置</Divider>
+              <DanmuViewConfigModifier />
+            </div>
+          );
+        }}
+      </ConfigContext.Consumer>
     );
   }
 }
