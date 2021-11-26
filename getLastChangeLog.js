180339627094f12b724c934a0dd05889844839ee
BiliGoldenWater <2439577268@qq.com>
2021-11-26 23:25:49 +0800

    finish get changelog

--- getLastChangeLog.js
+++ getLastChangeLog.js
@@ -6,11 +6,16 @@ const changeLog = fs.readFileSync("changeLog.md", {
   encoding: "utf-8",
 });
 
-const output = /[\s|\S]+?(?=\*\*\*)/
-  .exec(changeLog)[0]
-  .trimEnd()
-  .replace(/%/g, "%25")
-  .replace(/\n/g, "%0A")
-  .replace(/\r/g, "%0D");
+try {
+  const output = /[\s|\S]+?(?=\*\*\*)/
+    .exec(changeLog)[0]
+    .replace(/(([0-9]+)|(.| )+):/, "")
+    .trim()
+    .replace(/%/g, "%25")
+    .replace(/\n/g, "%0A")
+    .replace(/\r/g, "%0D");
 
-console.log("::set-output name=changelog::" + output);
+  console.log("::set-output name=changelog::" + output);
+} catch (e) {
+  console.log("::set-output name=changelog::empty");
+}
