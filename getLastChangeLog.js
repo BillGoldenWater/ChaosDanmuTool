aa7cf76e0e222307e6426b71d954bd64ebf7ceb4
BiliGoldenWater <2439577268@qq.com>
2021-11-26 22:56:17 +0800

    try another way to get change log

--- getLastChangeLog.js
+++ getLastChangeLog.js
@@ -6,7 +6,4 @@ const changeLog = fs.readFileSync("changeLog.md", {
   encoding: "utf-8",
 });
 
-console.log(
-  "::set-output name=output::" +
-    /[\s|\S]+?(?=\*\*\*)/.exec(changeLog)[0].trimEnd()
-);
+console.log(/[\s|\S]+?(?=\*\*\*)/.exec(changeLog)[0].trimEnd());
