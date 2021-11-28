5cdf2d6153ab7a053d143bbff3ec191b8c62b35b
BiliGoldenWater <2439577268@qq.com>
2021-11-28 21:25:23 +0800

    add load/save/reset to Settings.tsx update changeLog.md

--- src/window/main/page/settings/Settings.tsx
+++ src/window/main/page/settings/Settings.tsx
@@ -1,13 +1,47 @@
 import React, { ReactNode } from "react";
-import { Collapse } from "antd";
+import { Button, Collapse, Divider, message, Space } from "antd";
 import { MainConfigModifier } from "./configmodifier/mainconfigmodifier/MainConfigModifier";
 import { DanmuViewConfigModifier } from "./configmodifier/danmuviewconfigmodifier/DanmuViewConfigModifier";
 import { DanmuViewCustomsModifier } from "./configmodifier/danmuviewcustomsmodifier/DanmuViewCustomsModifier";
+import { ConfigContext } from "../../utils/ConfigContext";
+import { defaultConfig } from "../../../../utils/config/Config";
 
 export class Settings extends React.Component {
   render(): ReactNode {
     return (
       <div>
+        <ConfigContext.Consumer>
+          {({ setConfig }) => {
+            return (
+              <Space>
+                <Button
+                  onClick={() => {
+                    window.electron.loadConfig();
+                    setConfig(JSON.parse(window.electron.getConfig()));
+                  }}
+                >
+                  读取
+                </Button>
+                <Button
+                  onClick={() => {
+                    window.electron.saveConfig();
+                  }}
+                >
+                  保存
+                </Button>
+                <Button
+                  onClick={() => {
+                    setConfig(defaultConfig);
+                    message.success("重置成功").then();
+                  }}
+                >
+                  重置
+                </Button>
+              </Space>
+            );
+          }}
+        </ConfigContext.Consumer>
+        <Divider />
         <Collapse defaultActiveKey={"mainSettings"}>
           <Collapse.Panel key={"mainSettings"} header={"主要设置"}>
             <MainConfigModifier />
