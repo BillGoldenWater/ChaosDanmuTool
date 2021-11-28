4c177664610dcc036628142369aec1ca9604e11c
BiliGoldenWater <2439577268@qq.com>
2021-11-29 01:31:14 +0800

    localize connection status message update changeLog.md change version to 0.1.8

--- src/window/viewer/page/danmurender/danmuitem/item/statusupdate/StatusUpdate.tsx
+++ src/window/viewer/page/danmurender/danmuitem/item/statusupdate/StatusUpdate.tsx
@@ -10,9 +10,26 @@ class Props {
 export class StatusUpdate extends React.Component<Props> {
   render(): ReactNode {
     const rsu: ReceiverStatusUpdate = this.props.msg as ReceiverStatusUpdate;
+    let statusMsg = "NULL";
+
+    switch (rsu.data.status) {
+      case "open": {
+        statusMsg = "已连接";
+        break;
+      }
+      case "close": {
+        statusMsg = "未连接";
+        break;
+      }
+      case "error": {
+        statusMsg = "发生了错误";
+        break;
+      }
+    }
+
     return (
       <div className={style.StatusUpdate}>
-        直播间连接状态现在为: {rsu.data.status}
+        直播间连接状态现在为: {statusMsg}
       </div>
     );
   }
