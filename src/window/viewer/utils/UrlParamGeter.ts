4a3eca4e3074859b0f8c18bf2b1d6d58bb9f4759
BiliGoldenWater <2439577268@qq.com>
2022-01-01 05:32:56 +0800

    优化URL参数获取流程

--- src/window/viewer/utils/UrlParamGeter.ts
+++ src/window/viewer/utils/UrlParamGeter.ts
@@ -1,9 +1,3 @@
 export function getParam(key: string): string {
-  const paramStr: string = window.location.search.substring(1);
-  const paramList: string[] = paramStr.split("&");
-
-  for (const param in paramList) {
-    const [name, value] = paramList[param].split("=");
-    if (name == key) return value;
-  }
+  return new URLSearchParams(window.location.search).get(key);
 }
