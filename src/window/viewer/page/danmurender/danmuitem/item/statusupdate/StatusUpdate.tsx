d90b8bed0e0cd68d4614e9e16bf8fe742095165c
Golden_Water <2439577268@qq.com>
2022-03-27 02:57:39 +0800

    弹幕查看器接收状态更新时 不同状态以不同的醒目颜色显示

--- src/window/viewer/page/danmurender/danmuitem/item/statusupdate/StatusUpdate.tsx
+++ src/window/viewer/page/danmurender/danmuitem/item/statusupdate/StatusUpdate.tsx
@@ -15,28 +15,38 @@ export class StatusUpdate extends React.Component<Props> {
   render(): ReactNode {
     const rsu: ReceiverStatusUpdate = this.props.msg as ReceiverStatusUpdate;
     let statusMsg = "NULL";
+    let color = "#fff";
 
     switch (rsu.data.status) {
       case "connecting": {
         statusMsg = "连接中";
+        color = "#e90";
         break;
       }
       case "open": {
         statusMsg = "已连接";
+        color = "#0e0";
         break;
       }
       case "close": {
         statusMsg = "未连接";
+        color = "#ee0";
         break;
       }
       case "error": {
         statusMsg = "发生了错误";
+        color = "#e00";
         break;
       }
     }
 
     return (
-      <div className={style.StatusUpdate}>
+      <div
+        className={style.StatusUpdate}
+        style={{
+          color: color,
+        }}
+      >
         直播间连接状态现在为: {statusMsg}
       </div>
     );
