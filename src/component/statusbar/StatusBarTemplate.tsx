94694c5adb7f0cb9922919542390079c3f28d6f5
Golden_Water <2439577268@qq.com>
2022-03-31 08:23:59 +0800

    添加状态消息宽度限制

--- src/component/statusbar/StatusBarTemplate.tsx
+++ src/component/statusbar/StatusBarTemplate.tsx
@@ -2,7 +2,7 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 
-import React, { ReactNode } from "react";
+import React, { ReactNode, RefObject } from "react";
 import "./StatusBar.css";
 
 class Props {
@@ -12,12 +12,40 @@ class Props {
 }
 
 export class StatusBarTemplate extends React.Component<Props> {
+  contentRef: RefObject<HTMLDivElement> = React.createRef();
+  childrenRef: RefObject<HTMLDivElement> = React.createRef();
+
+  constructor(props: Props) {
+    super(props);
+  }
+
   render(): JSX.Element {
+    const contentWidth = this.contentRef.current
+      ? window.getComputedStyle(this.contentRef.current).width
+      : "0";
+
+    const childrenWidth = this.childrenRef.current
+      ? window.getComputedStyle(this.childrenRef.current).width
+      : "0";
+
     return (
       <div className={this.props.className}>
-        <div className="statusBar_content" style={this.props.style}>
-          <div>{this.props.message}</div>
-          <div className="statusBar_itemList">{this.props.children}</div>
+        <div
+          ref={this.contentRef}
+          className="statusBar_content"
+          style={this.props.style}
+        >
+          <div
+            className={"statusBar_statusMessage"}
+            style={{
+              maxWidth: `calc(${contentWidth} - ${childrenWidth})`,
+            }}
+          >
+            {this.props.message}
+          </div>
+          <div ref={this.childrenRef} className="statusBar_itemList">
+            {this.props.children}
+          </div>
         </div>
       </div>
     );
