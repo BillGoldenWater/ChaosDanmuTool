4f6040710ebcd62a0f0340ae631c8f55c0b0dc82
BiliGoldenWater <2439577268@qq.com>
2022-05-22 19:07:25 +0800

    Select 优化样式 优化动画

--- src/rendererShare/component/select/Select.tsx
+++ src/rendererShare/component/select/Select.tsx
@@ -102,11 +102,9 @@ export class Select<V> extends React.Component<Props<V>, State> {
       [
         {
           height: `${startRect.height}px`,
-          width: `${startRect.width}px`,
         },
         {
           height: `${finalRect.height}px`,
-          width: `${finalRect.width}px`,
         },
       ],
       {
