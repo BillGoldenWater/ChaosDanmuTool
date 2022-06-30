5c36fa2b72795c8da3c84ca26be7a457297e8aba
BiliGoldenWater <2439577268@qq.com>
2022-07-01 01:37:28 +0800

    优化代码样式

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -12,6 +12,58 @@ use tauri::{App, AppHandle, Manager, Wry};
 
 use chaosdanmutool::libs::utils::window_utils::set_visible_on_all_workspaces;
 
+fn main() {
+  let context = tauri::generate_context!();
+
+  // region init
+  let app = tauri::Builder::default()
+    .setup(|app| {
+      on_init(app);
+      Ok(())
+    })
+    // .invoke_handler(tauri::generate_handler![set_visible_on_all_workspace])
+    .menu(if cfg!(target_os = "macos") {
+      tauri::Menu::os_default("Chaos Danmu Tool")
+    } else {
+      tauri::Menu::default()
+    })
+    .build(context)
+    .expect("error while building tauri application");
+  // endregion
+
+  app
+    .run(|app_handle, event| match event {
+      tauri::RunEvent::Ready {} => { // ready event
+        on_ready(app_handle)
+      }
+      tauri::RunEvent::ExitRequested { api, .. } => { // exit requested event
+        #[cfg(target_os = "macos")]
+        {
+          println!("[RunEvent.ExitRequested] Exit prevented");
+          api.prevent_exit()
+        }
+      }
+
+      _ => {}
+    });
+}
+
+fn on_init(app: &mut App<Wry>) {
+  show_main_window(app.app_handle());
+}
+
+fn on_ready(_app_handle: &AppHandle<Wry>) {}
+
+fn show_main_window(app_handle: AppHandle<Wry>) {
+  let main_window = app_handle.get_window("main");
+
+  if let Some(main_window) = main_window {
+    main_window.show().expect("Failed to show main_window");
+  } else {
+    create_main_window(app_handle)
+  }
+}
+
 fn create_main_window(app_handle: AppHandle<Wry>) {
   let main_window = tauri::WindowBuilder
   ::new(&app_handle, "main", tauri::WindowUrl::App("index.html".into()))
@@ -30,53 +82,4 @@ fn create_main_window(app_handle: AppHandle<Wry>) {
                                 true,
                                 true,
                                 false);
-}
-
-fn show_main_window(app_handle: AppHandle<Wry>) {
-  let main_window = app_handle.get_window("main");
-
-  if let Some(main_window) = main_window {
-    main_window.show().expect("Failed to show main_window");
-  } else {
-    create_main_window(app_handle)
-  }
-}
-
-fn on_ready(_app_handle: &AppHandle<Wry>) {}
-
-fn on_init(app: &mut App<Wry>) {
-  show_main_window(app.app_handle());
-}
-
-fn main() {
-  let context = tauri::generate_context!();
-  let app = tauri::Builder::default()
-    .setup(|app| {
-      on_init(app);
-      Ok(())
-    })
-    // .invoke_handler(tauri::generate_handler![set_visible_on_all_workspace])
-    .menu(if cfg!(target_os = "macos") {
-      tauri::Menu::os_default("Chaos Danmu Tool")
-    } else {
-      tauri::Menu::default()
-    })
-    .build(context)
-    .expect("error while building tauri application");
-
-  app
-    .run(|app_handle, event| match event {
-      tauri::RunEvent::Ready {} => {
-        on_ready(app_handle)
-      }
-      tauri::RunEvent::ExitRequested { api, .. } => {
-        #[cfg(target_os = "macos")]
-        {
-          println!("[RunEvent.ExitRequested] Exit prevented");
-          api.prevent_exit()
-        }
-      }
-
-      _ => {}
-    });
 }
