729412a90907a81967969a6444500b6a1851733a
BiliGoldenWater <2439577268@qq.com>
2022-07-27 21:22:25 +0800

    添加 DanmuType

--- src-tauri/src/libs/command/command_packet/bilibili_command/danmu_message.rs
+++ src-tauri/src/libs/command/command_packet/bilibili_command/danmu_message.rs
@@ -18,7 +18,7 @@ pub struct DanmuMessage {
    * ms
    * */
   timestamp: u64,
-  danmu_type: i32,
+  danmu_type: DanmuType,
   emoji_data: EmojiData,
 
   content: String,
@@ -43,5 +43,24 @@ pub struct DanmuMessage {
 impl DanmuMessage {
   pub fn from_raw(_raw: Value) -> DanmuMessage {
     todo!()
+  }
+}
+
+#[derive(serde::Serialize, serde::Deserialize, ts_rs::TS, PartialEq, Debug, Clone)]
+#[serde(rename_all = "camelCase")]
+#[ts(export, export_to = "../src/share/type/rust/command/commandPacket/bilibiliCommand/danmuMessage/")]
+pub enum DanmuType {
+  Normal = 0,
+  Emoji = 1,
+  Unknown = 10086,
+}
+
+impl DanmuType {
+  pub fn from_u32(value: u32) -> DanmuType {
+    match value {
+      0 => DanmuType::Normal,
+      1 => DanmuType::Emoji,
+      _ => DanmuType::Unknown,
+    }
   }
 }
