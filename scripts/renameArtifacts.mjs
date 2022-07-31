1466072ccceeede966e3824fc7140b1f1e1260b7
BiliGoldenWater <2439577268@qq.com>
2022-08-01 00:59:43 +0800

    完成 renameArtifacts.mjs 修改 wix 的语言

--- scripts/renameArtifacts.mjs
+++ scripts/renameArtifacts.mjs
@@ -4,23 +4,6 @@
  */
 
 import fs from "fs/promises";
-import path from "path";
-
-/**
- * @param {string} dir
- */
-async function printDir(dir) {
-    for (let it of (await fs.readdir(dir))) {
-        it = path.join(dir, it)
-        let stat = await fs.stat(it)
-        if (stat.isFile()) {
-            console.log(`f: ${it}`)
-        } else if (stat.isDirectory()) {
-            console.log(`d: ${it}`)
-            await printDir(it)
-        }
-    }
-}
 
 async function main() {
     // noinspection JSFileReferences
@@ -31,28 +14,51 @@ async function main() {
 
     switch (process.platform) {
         case "darwin": {
-            const dmgPath =
-                `src-tauri/target/universal-apple-darwin/release/bundle/dmg/ChaosDanmuTool_${version}_universal.dmg`
+            const target = "universal-apple-darwin"
+            const pkgPath =
+                `src-tauri/target/${target}/release/bundle/dmg/ChaosDanmuTool_${version}_universal.dmg`
             const updatePkgPath =
-                `src-tauri/target/universal-apple-darwin/release/bundle/macos/ChaosDanmuTool.app.tar.gz`
+                `src-tauri/target/${target}/release/bundle/macos/ChaosDanmuTool.app.tar.gz`
             const sigPath =
-                `src-tauri/target/universal-apple-darwin/release/bundle/macos/ChaosDanmuTool.app.tar.gz.sig`
+                `src-tauri/target/${target}/release/bundle/macos/ChaosDanmuTool.app.tar.gz.sig`
 
-            await fs.copyFile(dmgPath, `out/ChaosDanmuTool_${version}_universal.dmg`)
-            await fs.copyFile(updatePkgPath, `out/ChaosDanmuTool_${version}_universal.update.tar.gz`)
-            await fs.copyFile(sigPath, `out/ChaosDanmuTool_${version}_universal.update.sig`)
+            await fs.copyFile(pkgPath, `out/macos/ChaosDanmuTool_${version}_universal.dmg`)
+            await fs.copyFile(updatePkgPath, `out/macos/ChaosDanmuTool_${version}_universal.update.tar.gz`)
+            await fs.copyFile(sigPath, `out/macos/ChaosDanmuTool_${version}_universal.update.sig`)
 
             break
         }
         case "linux": {
+            const target = "x86_64-unknown-linux-gnu"
+            const pkgPath =
+                `src-tauri/target/${target}/release/bundle/deb/chaos-danmu-tool_${version}_amd64.deb`
+            const updatePkgPath =
+                `src-tauri/target/${target}/release/bundle/appimage/chaos-danmu-tool_${version}_amd64.AppImage.tar.gz`
+            const sigPath =
+                `src-tauri/target/${target}/release/bundle/appimage/chaos-danmu-tool_${version}_amd64.AppImage.tar.gz.sig`
 
-            await printDir("src-tauri/target/x86_64-unknown-linux-gnu/release/bundle/")
+            await fs.copyFile(pkgPath, `out/linux/chaos-danmu-tool_${version}_amd64.deb`)
+            await fs.copyFile(updatePkgPath, `out/linux/chaos-danmu-tool_${version}_amd64.update.tar.gz`)
+            await fs.copyFile(sigPath, `out/linux/chaos-danmu-tool_${version}_amd64.update.sig`)
 
             break
         }
         case "win32": {
+            const tauriConfig = JSON.parse((await fs.readFile("./src-tauri/tauri.conf.json")).toString())
+            // noinspection JSUnresolvedVariable
+            const wL = tauriConfig?.tauri?.bundle?.windows?.wix?.language || "en-US"
 
-            await printDir("src-tauri/target/x86_64-pc-windows-msvc/release/bundle/")
+            const target = "x86_64-pc-windows-msvc"
+            const pkgPath =
+                `src-tauri/target/${target}/release/bundle/msi/ChaosDanmuTool_${version}_x64_${wL}.msi`
+            const updatePkgPath =
+                `src-tauri/target/${target}/release/bundle/msi/ChaosDanmuTool_${version}_x64_${wL}.update.zip`
+            const sigPath =
+                `src-tauri/target/${target}/release/bundle/msi/ChaosDanmuTool_${version}_x64_${wL}.msi.zip.sig`
+
+            await fs.copyFile(pkgPath, `out/windows/ChaosDanmuTool_${version}_x64_${wL}.msi`)
+            await fs.copyFile(updatePkgPath, `out/windows/ChaosDanmuTool_${version}_x64_${wL}.update.zip`)
+            await fs.copyFile(sigPath, `out/windows/ChaosDanmuTool_${version}_x64_${wL}.update.sig`)
 
             break
         }
