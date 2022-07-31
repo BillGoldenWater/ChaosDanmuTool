1c1d961652e1fef6f3dfa8abad89b1f7c2b0139e
BiliGoldenWater <2439577268@qq.com>
2022-07-31 23:10:55 +0800

    添加 beforeBuild 的高亮输出

--- scripts/beforeBuild.mjs
+++ scripts/beforeBuild.mjs
@@ -16,13 +16,31 @@ function execCommand(command, args, cwd = undefined,) {
     return spawnSync(command, args, {cwd, stdio: 'inherit',})
 }
 
+/**
+ * @param {string} msg
+ */
+function highlightLog(msg) {
+    console.log(`\u001b[94m${msg}\u001b[0m`);
+}
+
 async function main() {
+    // region test
+    highlightLog("cargo test")
+
     await fs.rm("src/share/type/rust", {recursive: true, force: true})
     execCommand("cargo", ["test"], "src-tauri")
+    // endregion
+
+    // region add macOS targets
+    highlightLog("add targets")
 
     if (process.platform === "darwin") {
         execCommand("rustup", ["target", "add", "x86_64-apple-darwin", "aarch64-apple-darwin"])
     }
+    // endregion
+
+    // region rm last bundle output
+    highlightLog("rm last bundle output")
 
     const bundleDirs = [
         "./src-tauri/target/universal-apple-darwin/release/bundle/",
@@ -35,6 +53,7 @@ async function main() {
         } catch (e) {
         }
     }
+    // endregion
 }
 
 main().then()
