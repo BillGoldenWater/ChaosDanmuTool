1261798003c7d0a63a294b4cb901b18c88ab0794
BiliGoldenWater <2439577268@qq.com>
2022-08-06 22:13:38 +0800

    添加 Layout 组件

---
+++ src/share/component/Layout.svelte
@@ -0,0 +1,34 @@
+<!--
+  - Copyright 2021-2022 Golden_Water
+  - SPDX-License-Identifier: AGPL-3.0-only
+  -->
+<script lang="ts">
+</script>
+
+<div class="root">
+  <div class="sider">
+    <slot name="sider" />
+  </div>
+  <div class="content">
+    <slot name="content">Content</slot>
+  </div>
+</div>
+
+<style lang="less">
+  .root {
+    display: flex;
+  }
+
+  .height {
+    height: calc(100vh - var(--spacerWidthHalf) * 2);
+  }
+
+  .sider {
+    .height();
+  }
+
+  .content {
+    .height();
+    flex-grow: 1;
+  }
+</style>
