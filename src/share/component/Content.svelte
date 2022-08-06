6c20018e5297f10b5488e38fa123f940bb727f7b
BiliGoldenWater <2439577268@qq.com>
2022-08-06 22:48:06 +0800

    添加 Content 组件

---
+++ src/share/component/Content.svelte
@@ -0,0 +1,32 @@
+<!--
+  - Copyright 2021-2022 Golden_Water
+  - SPDX-License-Identifier: AGPL-3.0-only
+  -->
+<script lang="ts">
+  export let disablePadding: boolean = false;
+  export let disableRound: boolean = false;
+</script>
+
+<div class="content" class:disablePadding class:disableRound>
+  <slot />
+</div>
+
+<style>
+  .content {
+    margin: var(--spacerWidthHalf);
+    padding: var(--spacerWidthHalf);
+
+    height: calc(100vh - var(--spacerWidth) * 2);
+    border-radius: var(--contentBorderRadius);
+
+    background-color: var(--up);
+  }
+
+  .disablePadding {
+    padding: 0;
+  }
+
+  .disableRound {
+    border-radius: 0;
+  }
+</style>
