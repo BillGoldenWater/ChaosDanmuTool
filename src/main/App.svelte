4adf6829544f45775af098b9aa2248f37821ffe7
BiliGoldenWater <2439577268@qq.com>
2022-08-07 23:18:06 +0800

    完成 Sider.svelte

--- src/main/App.svelte
+++ src/main/App.svelte
@@ -3,9 +3,35 @@
   - SPDX-License-Identifier: AGPL-3.0-only
   -->
 <script lang="ts">
+  import Layout from "../share/component/Layout.svelte";
+  import Content from "../share/component/Content.svelte";
+  import Sider from "../share/component/sider/Sider.svelte";
+  import VscDashboard from "svelte-icons-pack/vsc/VscDashboard";
+  import VscMenu from "svelte-icons-pack/vsc/VscMenu";
+  import VscHistory from "svelte-icons-pack/vsc/VscHistory";
+  import VscSettingsGear from "svelte-icons-pack/vsc/VscSettingsGear";
+  import VscInfo from "svelte-icons-pack/vsc/VscInfo";
+  import type { TSiderItem } from "../share/component/sider/TSiderItem";
+
+  let siderItems: TSiderItem[] = [
+    { key: "overview", icon: VscDashboard },
+    { key: "function", icon: VscMenu },
+    { key: "history", icon: VscHistory },
+    { key: "setting", icon: VscSettingsGear },
+    { key: "about", icon: VscInfo },
+  ];
 </script>
 
-<div class="app">main_window</div>
+<div class="app">
+  <Layout>
+    <div slot="sider">
+      <Sider items={siderItems} />
+    </div>
+    <div slot="content">
+      <Content>Content</Content>
+    </div>
+  </Layout>
+</div>
 
 <style>
   .app {
---
+++ src/share/component/sider/TSiderItem.ts
@@ -0,0 +1,10 @@
+/*
+ * Copyright 2021-2022 Golden_Water
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+export interface TSiderItem {
+  key: string;
+  icon: unknown;
+  element?: HTMLDivElement;
+}
