4b1b37b56eca74f5dd5466be090159a88db6ce03
BiliGoldenWater <2439577268@qq.com>
2022-08-07 23:17:53 +0800

    添加 theme themeDouble themeDown themeText

--- src/share/style/ThemeUtils.ts
+++ src/share/style/ThemeUtils.ts
@@ -19,7 +19,7 @@ function g(key: string): string {
 function updateVariables(colorPlates: ColorPlates) {
   console.log(colorPlates);
   Object.keys(colorPlates).forEach((key) => {
-    s(`--${key}`, colorPlates[key]);
+    s(`--${key}`, colorPlates[key].toString());
   });
 }
 
@@ -29,16 +29,21 @@ type ThemeSettings = {
 };
 
 type ColorPlates = {
-  text: string;
+  text: Color;
 
-  background: string;
+  background: Color;
 
-  up: string;
-  upDouble: string;
-  down: string;
-  downDouble: string;
+  up: Color;
+  upDouble: Color;
+  down: Color;
+  downDouble: Color;
 
-  themeSolid: string;
+  theme: Color;
+  themeDouble: Color;
+  themeDown: Color;
+
+  themeSolid: Color;
+  themeText: Color;
 };
 
 const defaultThemeSettings: ThemeSettings = {
@@ -49,37 +54,80 @@ const defaultThemeSettings: ThemeSettings = {
 const genColorPlates = (themeSettings: ThemeSettings): ColorPlates => {
   let tc = themeSettings.themeColor;
 
-  function down(percent: number) {
+  function getDown(percent: number) {
     return new Color([0, 0, 0, percent], "hsl");
   }
 
-  function up(percent: number) {
+  function getUp(percent: number) {
     return new Color([0, 0, 100, percent], "hsl");
   }
 
-  function theme(percent: number) {
+  function getTheme(percent: number) {
     return new Color(
       [tc.hue(), tc.saturationl(), tc.lightness(), percent],
       "hsl"
     );
   }
 
+  let text = getUp(0.9);
+
+  let background = getTheme(1).desaturate(0.9).darken(0.9).alpha(0.8);
+
+  let up = getUp(0.1);
+  let upDouble = getUp(0.2);
+  let down = getDown(0.1);
+  let downDouble = getDown(0.2);
+
+  let theme = getTheme(0.1);
+  let themeDouble = getTheme(0.2);
+  let themeDown = getTheme(0.1).mix(down);
+
+  let themeSolid = getTheme(1);
+  let themeText = getTheme(0.9);
+
+  if (!themeSettings.dark) {
+    text = getDown(0.9);
+
+    background = getTheme(1).desaturate(0.97).lighten(0.6).alpha(0.8);
+
+    up = getUp(0.4);
+    upDouble = getUp(0.8);
+    down = getDown(0.4);
+    downDouble = getDown(0.8);
+
+    theme = getTheme(0.1);
+    themeDouble = getTheme(0.2);
+    themeDown = getTheme(0.1).mix(down);
+
+    themeSolid = getTheme(1);
+    themeText = getTheme(0.9);
+  }
+
   return {
-    text: up(0.9).toString(),
-
-    background: theme(1).desaturate(0.9).darken(0.9).alpha(0.95).toString(),
-
-    up: theme(1).desaturate(0.9).lighten(0.9).alpha(0.1).toString(),
-    upDouble: theme(1).desaturate(0.9).lighten(0.9).alpha(0.2).toString(),
-    down: theme(1).desaturate(0.9).darken(0.9).alpha(0.1).toString(),
-    downDouble: theme(1).desaturate(0.9).darken(0.9).alpha(0.2).toString(),
-
-    themeSolid: theme(1).toString(),
+    text: text,
+    background: background,
+    up: up,
+    upDouble: upDouble,
+    down: down,
+    downDouble: downDouble,
+    theme: theme,
+    themeDouble: themeDouble,
+    themeDown: themeDown,
+    themeSolid: themeSolid,
+    themeText: themeText,
   };
 };
 
 const themeSettings = writable(defaultThemeSettings);
 
+let currentSettings: ThemeSettings = undefined;
 themeSettings.subscribe((themeSettings) => {
   updateVariables(genColorPlates(themeSettings));
+  currentSettings = themeSettings;
 });
+
+// @ts-ignore
+window.toggleTheme = function () {
+  currentSettings && (currentSettings.dark = !currentSettings.dark);
+  themeSettings.set(currentSettings);
+};
