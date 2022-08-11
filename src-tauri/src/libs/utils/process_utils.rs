d80c0354ffe68a27d77a515276bb408dcf9062cf
BiliGoldenWater <2439577268@qq.com>
2022-08-11 12:23:41 +0800

    添加 is_running_under_rosetta

--- src-tauri/src/libs/utils/process_utils.rs
+++ src-tauri/src/libs/utils/process_utils.rs
@@ -18,3 +18,24 @@ pub fn get_psn() -> ProcessSerialNumber {
     lowLongOfPSN: 2,
   }
 }
+
+#[cfg(target_os = "macos")]
+pub fn is_running_under_rosetta() -> std::io::Result<bool> {
+  unsafe {
+    let mut ret = 0;
+
+    let attr_name = std::ffi::CString::new("sysctl.proc_translated").unwrap();
+
+    let error = libc::sysctlbyname(
+      attr_name.as_ptr(),
+      &mut ret as *mut std::os::raw::c_int as *mut std::ffi::c_void, &mut 8,
+      std::ptr::null_mut(), 0,
+    );
+
+    if error < std::os::raw::c_int::default() {
+      return Err(std::io::Error::last_os_error());
+    }
+
+    Ok(ret == 1)
+  }
+}
