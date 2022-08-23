3b24d7a65bd0411bc3c5f8239f0dafb746161472
BiliGoldenWater <2439577268@qq.com>
2022-08-23 20:16:12 +0800

    完成 ConnectRoom.svelte

--- src/main/page/functions/ConnectRoom.svelte
+++ src/main/page/functions/ConnectRoom.svelte
@@ -5,9 +5,38 @@
 <script lang="ts">
   import Content from "../../../share/component/Content.svelte";
   import Button from "../../../share/component/Button.svelte";
+  import ConfigItem from "../../../share/component/ConfigItem.svelte";
+  import Spacer from "../../../share/component/Spacer.svelte";
+  import { backend } from "../../backendApi";
+  import { appEnv } from "../../../share/appEnv/AppEnv";
+
+  $: closed = $appEnv.receiverStatus === "close";
 </script>
 
 <Content>
-  房间号:
-  <Button>连接</Button>
+  <div>
+    <ConfigItem
+      key="backend.danmuReceiver.roomid"
+      type="number"
+      name="房间号"
+      description="B站直播房间号"
+      props={{ min: 0 }}
+    />
+    <Button
+      primary
+      on:click={() => {
+        closed
+          ? backend && backend.connectRoom()
+          : backend && backend.disconnectRoom();
+      }}
+      >{closed ? "连接" : "断开"}
+    </Button>
+  </div>
+  <Spacer size="double" direction="vertical" />
+  <ConfigItem
+    key="backend.danmuReceiver.autoReconnect"
+    type="switch"
+    name="自动重连"
+    description="在非正常断开连接的情况下自动重连"
+  />
 </Content>
