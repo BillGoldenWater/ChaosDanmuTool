fd1ed936708fd28d30e1b81962a53d1585fc97a1
BiliGoldenWater <2439577268@qq.com>
2022-12-12 23:01:29 +0800

    AppCtx.tsx 修复在浏览器中打开时无法连接至后端的问题

--- src/share/app/AppCtx.tsx
+++ src/share/app/AppCtx.tsx
@@ -29,7 +29,7 @@ import {
 } from "../event/AppEventTarget";
 import { TObjGet, TObjGetAndSet, TObjSet } from "../type/TGetAndSet";
 import { AppPath, TAppPath } from "./AppPath";
-import { TWindow } from "../../window/Page";
+import { TWindow } from "../../window/Window";
 import { TUserInfoCache } from "../type/TUserInfoCache";
 import { CommandReceiver } from "./CommandReceiver";
 import { backend } from "./BackendApi";
@@ -309,7 +309,8 @@ export function initialAppConstant(
     port: firstConfig.backend.httpServer.port,
   });
 
-  if (params.windowId === "viewer") {
+  // in viewer or open in browser
+  if (params.windowId === "viewer" || !backend) {
     receiver.updateOption({ host: window.location.hostname });
   }
 
