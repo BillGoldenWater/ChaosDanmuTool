72f88e02b739d768f8b6411468c5a66a0279923c
BiliGoldenWater <2439577268@qq.com>
2022-12-16 00:00:54 +0800

    添加 Spacer.tsx

---
+++ src/share/component/Spacer.tsx
@@ -0,0 +1,18 @@
+/*
+ * Copyright 2021-2022 Golden_Water
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+import styled from "styled-components";
+
+export interface SpacerProps {
+  vertical?: boolean;
+
+  size: string;
+}
+
+export const Spacer = styled.div<SpacerProps>`
+  display: flex;
+
+  ${(p) => (p.vertical ? `height: ${p.size}` : `width: ${p.size}`)}
+`;
