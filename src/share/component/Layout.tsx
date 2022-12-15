83daaff4302b9c632291d84c0733f992372d2060
BiliGoldenWater <2439577268@qq.com>
2022-12-16 04:46:35 +0800

    Layout.tsx 添加切换动效

--- src/share/component/Layout.tsx
+++ src/share/component/Layout.tsx
@@ -3,13 +3,14 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 
-import { PropsWithChildren, useContext } from "react";
+import { PropsWithChildren, useContext, useRef } from "react";
 import styled, { keyframes } from "styled-components";
 import { Icon } from "@iconscout/react-unicons";
 import { Tooltip } from "./Tooltip";
 import { Spacer } from "./Spacer";
 import { themeCtx } from "./ThemeCtx";
 import Color from "color";
+import { AnimatePresence, motion } from "framer-motion";
 
 const layoutSpacing = "0.8em";
 const menuSpacing = "0.4em";
@@ -17,6 +18,7 @@ const menuSpacing = "0.4em";
 // region layout
 export interface LayoutProps {
   menu: MenuProps;
+  extraPageIdent?: string; // for same page but switch content
 }
 
 const LayoutBase = styled.div`
@@ -27,19 +29,63 @@ const LayoutBase = styled.div`
   padding: ${layoutSpacing};
 `;
 
-const LayoutContent = styled.div`
+const LayoutContent = styled(motion.div)`
   padding: ${layoutSpacing};
   border-radius: 0.5em;
 
   background-color: ${(p) => p.theme.consts.contentBackground};
+  height: 100%;
   flex-grow: 1;
+
+  overflow-y: auto;
 `;
 
-export function Layout({ menu, children }: PropsWithChildren<LayoutProps>) {
+export function Layout({
+  menu,
+  extraPageIdent,
+  children,
+}: PropsWithChildren<LayoutProps>) {
+  const ident = `${menu.selected} ${extraPageIdent}`;
+
+  const prevSel = useRef(menu.selected);
+
+  // null for same page
+  let switchDown: boolean | null = null;
+  if (prevSel.current !== menu.selected) {
+    const prevIndex = menu.menuItems.findIndex(
+      (it) => it.itemKey === prevSel.current
+    );
+    const curIndex = menu.menuItems.findIndex(
+      (it) => it.itemKey === menu.selected
+    );
+    switchDown = prevIndex < curIndex;
+    prevSel.current = menu.selected;
+  }
+
+  const extraIdentSet =
+    extraPageIdent !== undefined &&
+    /* ignore switch from other page */ switchDown == null;
+  // doesn't need to detect ident change because it will be filter by react prop compare
+
+  // when extraPageIdent is set, scroll from right
+  // otherwise depends on is or not switching down
+  const initialY = extraIdentSet ? "" : switchDown ? "100%" : "-100%";
+  const initialX = extraIdentSet ? "100%" : "";
+
   return (
     <LayoutBase>
       <Menu {...menu} />
-      <LayoutContent>{children}</LayoutContent>
+      <AnimatePresence mode={"popLayout"} initial={false}>
+        <LayoutContent
+          key={ident}
+          initial={{ y: initialY, x: initialX, opacity: 0 }}
+          animate={{ y: 0, x: 0, opacity: 1 }}
+          exit={{ y: 0, x: "-50%", opacity: 0, scale: 0, filter: "blur(1em)" }}
+          transition={{ duration: 0.3, ease: "easeOut" }}
+        >
+          {children}
+        </LayoutContent>
+      </AnimatePresence>
     </LayoutBase>
   );
 }
@@ -56,7 +102,7 @@ export interface TMenuItem {
 }
 
 export interface MenuProps {
-  defaultKey: string;
+  selected: string;
   menuItems: TMenuItem[];
   menuOnClick: (key: string) => void;
 }
@@ -75,14 +121,14 @@ const MenuBase = styled.div`
   //overflow: hidden; weird issue
 `;
 
-function Menu({ defaultKey, menuItems, menuOnClick }: MenuProps) {
+function Menu({ selected, menuItems, menuOnClick }: MenuProps) {
   return (
     <MenuBase>
       {menuItems.map((it) => (
         <div key={it.itemKey}>
           <MenuItem
             {...it}
-            active={defaultKey === it.itemKey}
+            active={selected === it.itemKey}
             onClick={menuOnClick}
           />
           <Spacer size={menuSpacing} vertical />
