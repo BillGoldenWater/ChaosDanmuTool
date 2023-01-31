257dfb9f8e4e3bb898856e3fb59206ef1bc23a52
BiliGoldenWater <2439577268@qq.com>
2023-02-01 03:45:05 +0800

    console_utils.rs 添加 ansi 代码的支持

--- src-tauri/src/utils/console_utils.rs
+++ src-tauri/src/utils/console_utils.rs
@@ -9,6 +9,7 @@ pub fn attach_console() {
     use windows::Win32::System::Console::{AttachConsole, ATTACH_PARENT_PROCESS};
     unsafe {
       AttachConsole(ATTACH_PARENT_PROCESS);
+      enable_virtual_terminal_processing();
     }
   }
 }
@@ -29,6 +30,27 @@ pub fn alloc_console() {
     use windows::Win32::System::Console::AllocConsole;
     unsafe {
       AllocConsole();
+      enable_virtual_terminal_processing();
+    }
+  }
+}
+
+#[cfg(target_os = "windows")]
+fn enable_virtual_terminal_processing() {
+  use windows::Win32::System::Console::{
+    GetConsoleMode, GetStdHandle, SetConsoleMode, CONSOLE_MODE, ENABLE_PROCESSED_OUTPUT,
+    ENABLE_VIRTUAL_TERMINAL_PROCESSING, STD_OUTPUT_HANDLE,
+  };
+
+  unsafe {
+    let console = GetStdHandle(STD_OUTPUT_HANDLE);
+    if let Ok(h_console) = console {
+      let mut console_mode: CONSOLE_MODE = CONSOLE_MODE(0);
+      GetConsoleMode(h_console, &mut console_mode);
+      SetConsoleMode(
+        h_console,
+        console_mode | ENABLE_PROCESSED_OUTPUT | ENABLE_VIRTUAL_TERMINAL_PROCESSING,
+      );
     }
   }
 }
