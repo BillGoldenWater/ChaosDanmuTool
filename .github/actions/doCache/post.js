68fc4ee4545952e74bef7a472da338944dfc3f13
BiliGoldenWater <2439577268@qq.com>
2023-02-21 19:17:36 +0800

    feat(GitHub Action): 在缓存更新时删除旧缓存

--- .github/actions/doCache/post.js
+++ .github/actions/doCache/post.js
@@ -5,6 +5,9 @@
 
 const core = require("@actions/core");
 const cache = require("@actions/cache");
+const { Octokit } = require("@octokit/core");
+const { createActionAuth } = require("@octokit/auth-action");
+
 const { gen } = require("./genCacheInfos.mjs");
 
 /**
@@ -14,12 +17,35 @@ function highlightLog(msg) {
   console.log(`\u001b[94m${msg}\u001b[0m`);
 }
 
+async function deleteCache(octokit, key) {
+  highlightLog(`deleting cache ${key}`);
+  let res = await octokit.request(
+    "DELETE /repos/{owner}/{repo}/actions/caches/{cache_id}",
+    {
+      owner: "BillGoldenWater",
+      repo: "ChaosDanmuTool",
+      cache_id: key,
+    }
+  );
+  if (res.status === 204) {
+    highlightLog(`delete success with response code ${res.status}`);
+  } else {
+    highlightLog(`delete failed with response`);
+    console.log(res);
+  }
+}
+
 async function main() {
   try {
+    const actionOctokit = new Octokit({
+      authStrategy: createActionAuth,
+    });
+
     let cacheItems = gen();
 
     for (let cacheItem of cacheItems) {
-      if (core.getState(`${cacheItem.id}_cacheKey`) === cacheItem.key) {
+      const oldKey = core.getState(`${cacheItem.id}_cacheKey`);
+      if (oldKey === cacheItem.key) {
         highlightLog(`cache ${cacheItem.key} exists, skipped.`);
         continue;
       }
@@ -28,6 +54,9 @@ async function main() {
       try {
         await cache.saveCache(cacheItem.paths, cacheItem.key);
         highlightLog(`cache ${cacheItem.key} saved`);
+        if (oldKey !== undefined) {
+          await deleteCache(actionOctokit, oldKey);
+        }
       } catch (e) {
         console.log(e);
         highlightLog(`failed to save cache ${cacheItem.key}`);
