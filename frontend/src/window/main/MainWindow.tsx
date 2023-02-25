3da72a1f1776ecaf994e9ea240972cd9833c4f85
BiliGoldenWater <2439577268@qq.com>
2023-02-25 22:28:17 +0800

    fix(frontend::main): 完成基本布局

--- frontend/src/window/main/MainWindow.tsx
+++ frontend/src/window/main/MainWindow.tsx
@@ -3,14 +3,45 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 
+import { LayoutGroup, motion } from "framer-motion";
 import React from "react";
 import { Background } from "../../share/component/Background";
 import { ConnectPanel } from "./ConnectPanel";
+import { paddingValue } from "../../share/component/ThemeCtx";
 
 export function MainWindow() {
   return (
-    <Background>
-      <ConnectPanel />
+    <Background
+      css={`
+        display: flex;
+        flex-direction: column;
+
+        gap: ${paddingValue.window};
+      `}
+    >
+      <LayoutGroup>
+        <ConnectPanel />
+        <motion.div
+          layout={"preserve-aspect"}
+          css={`
+            display: flex;
+            background-color: gray;
+            flex-direction: column-reverse;
+            flex-grow: 1;
+          `}
+        >
+          123
+        </motion.div>
+        <motion.div
+          layout
+          css={`
+            background-color: gray;
+          `}
+          whileHover={{ padding: paddingValue.normal }}
+        >
+          Toolbar
+        </motion.div>
+      </LayoutGroup>
     </Background>
   );
 }
