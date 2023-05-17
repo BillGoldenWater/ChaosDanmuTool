8da0b1db155061acfbe4a13f85e06c0269fd4f5e
Golden_Water <2439577268@qq.com>
2023-05-18 00:27:07 +0800

    feat(frontend::component::userMessage): 重构渲染逻辑; 优化样式

--- frontend/src/share/component/danmuItem/UserMessage.tsx
+++ frontend/src/share/component/danmuItem/UserMessage.tsx
@@ -3,11 +3,12 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 
-import { PropsWithChildren, useContext } from "react";
+import { PropsWithChildren, useContext, useMemo } from "react";
 import { appCtx } from "../../app/AppCtx";
 import styled, { css } from "styled-components";
 import { UserAvatar } from "../userInfo/UserAvatar";
 import { UserInfo } from "../userInfo/UserInfo";
+import { UserInfo as TUserInfo } from "../../type/rust/types/user_info";
 import {
   border,
   color,
@@ -22,59 +23,90 @@ export interface UserMessageProps {
   uid: string;
   mergePrev: boolean;
   mergeNext: boolean;
-  timestamp?: string;
+  timestamp: string;
   highlightColor: string;
 }
 
 export function UserMessage(props: PropsWithChildren<UserMessageProps>) {
   const { children, uid, timestamp, mergePrev, mergeNext, highlightColor } =
     props;
-
   const ctx = useContext(appCtx);
-
+  const userInfo = ctx.getUserInfo(uid);
   const compact = false;
 
-  let sider = <></>;
-  let msgUserInfo = <></>;
-
-  const userInfo = ctx.getUserInfo(uid);
-
-  if (!mergePrev) {
-    sider = <UserAvatar userInfo={userInfo} size={"40rem"} />;
-    msgUserInfo = (
-      <MessageUserInfo>
-        <UserInfo userInfo={userInfo} showAvatar={compact} />
-      </MessageUserInfo>
+  // region sider
+  const sider = useMemo(() => {
+    if (compact) return null;
+    return (
+      <MessageSider
+        userInfo={userInfo}
+        timestamp={timestamp}
+        isAvatar={!mergePrev}
+      />
     );
-  } else if (timestamp) {
-    const ts = new Date(Number.parseInt(timestamp));
-    const timeStr = formatTime(ts, "{hours}:{minutes}");
-    sider = <MessageTimestamp>{timeStr}</MessageTimestamp>;
-  }
+  }, [compact, mergePrev, timestamp, userInfo]);
+  // endregion
+
+  // region msgUserInfo
+  const msgUserInfo = useMemo(() => {
+    if (mergePrev) return null;
+    return (
+      <div>
+        <UserInfo userInfo={userInfo} showAvatar={compact} />
+      </div>
+    );
+  }, [compact, mergePrev, userInfo]);
+  // endregion
 
   return (
-    <UserMessageBase
+    <UserMessageContainer
       $mergePrev={mergePrev}
       $mergeNext={mergeNext}
       $highlightColor={highlightColor}
     >
-      {!compact && <MessageSider $isAvatar={!mergePrev}>{sider}</MessageSider>}
+      {sider}
       <MessageMain>
         {msgUserInfo}
         <MessageContent>{children}</MessageContent>
       </MessageMain>
-    </UserMessageBase>
+    </UserMessageContainer>
   );
 }
 
-const MessageSider = styled.div<{ $isAvatar: boolean }>`
-  display: flex;
+function MessageSider(props: {
+  isAvatar: boolean;
+  userInfo: TUserInfo;
+  timestamp: string;
+}) {
+  const { isAvatar, userInfo, timestamp } = props;
 
-  ${(p) =>
-    p.$isAvatar ? "align-items: flex-start;" : "align-items: flex-end;"};
-  justify-content: ${(p) => (p.$isAvatar ? "start" : "center")};
+  const content = useMemo(() => {
+    if (isAvatar) {
+      return <UserAvatar userInfo={userInfo} size={"40rem"} />;
+    } else {
+      const ts = new Date(Number.parseInt(timestamp));
+      const timeStr = formatTime(ts, "{hours}:{minutes}");
+      return <MessageTimestamp>{timeStr}</MessageTimestamp>;
+    }
+  }, [isAvatar, userInfo, timestamp]);
+
+  return (
+    <MessageSiderContainer $isAvatar={isAvatar}>
+      {content}
+    </MessageSiderContainer>
+  );
+}
+
+const MessageSiderContainer = styled.div<{ $isAvatar: boolean }>`
+  display: flex;
+  flex-direction: column;
+
+  align-items: ${(p) => (p.$isAvatar ? "" : "center")};
+  justify-content: ${(p) => (p.$isAvatar ? "" : "center")};
 
   min-width: 40rem;
+
+  container-type: size;
 `;
 
 const MessageTimestamp = styled.div`
@@ -82,18 +114,21 @@ const MessageTimestamp = styled.div`
   display: flex;
   align-items: center;
 
+  height: 100%;
+
   color: ${color.txtSecond};
 
-  height: min(16rem, 100%);
+  // note: intellij formatter would break this (WEB-59064)
+  @container (height > 20rem) {
+    align-items: flex-end;
+  }
 `;
 
 const MessageMain = styled.div`
   display: flex;
   flex-direction: column;
-  gap: ${paddingValue.small};
+  gap: calc(${paddingValue.small} / 2);
 `;
-
-const MessageUserInfo = styled.div``;
 
 const MessageContent = styled.div`
   ${font.dmContent};
@@ -108,7 +143,23 @@ interface UserMessageBaseProps {
   $highlightColor: string;
 }
 
-const UserMessageBase = styled.div<UserMessageBaseProps>`
+const styleMergePrev = css`
+  padding-top: calc(${paddingValue.small} / 2);
+
+  border-top-width: 0;
+  border-top-left-radius: 0;
+  border-top-right-radius: 0;
+`;
+
+const styleMergeNext = css`
+  padding-bottom: calc(${paddingValue.small} / 2);
+
+  border-bottom-width: 0;
+  border-bottom-left-radius: 0;
+  border-bottom-right-radius: 0;
+`;
+
+const UserMessageContainer = styled.div<UserMessageBaseProps>`
   transition: background-color 0.15s ease-in-out;
   display: flex;
   gap: ${paddingValue.small};
@@ -116,30 +167,11 @@ const UserMessageBase = styled.div<UserMessageBaseProps>`
   ${radius.normal};
   ${border.normal};
   ${padding.small};
+  ${(p) => (!p.$mergePrev ? `margin-top: ${paddingValue.small};` : "")}
 
   border-color: ${(p) => p.$highlightColor};
-  ${(p) => {
-    if (p.$mergePrev) {
-      return css`
-        border-top-width: 0;
-        border-top-left-radius: 0;
-        border-top-right-radius: 0;
-      `;
-    } else {
-      return `margin-top: ${paddingValue.small};`;
-    }
-  }};
-  ${(p) => {
-    if (p.$mergeNext) {
-      return css`
-        border-bottom-width: 0;
-        border-bottom-left-radius: 0;
-        border-bottom-right-radius: 0;
-      `;
-    } else {
-      return "";
-    }
-  }};
+  ${(p) => (p.$mergePrev ? styleMergePrev : "")};
+  ${(p) => (p.$mergeNext ? styleMergeNext : "")};
 
   background-color: transparent;
 
