4f41371e470b8f10de3ed93f9b9fcbf0e782afa8
Golden_Water <2439577268@qq.com>
2024-03-23 00:25:00 +0800

    feat(client::client::client_config): hide secret key

--- client/src/client/client_config.rs
+++ client/src/client/client_config.rs
@@ -1,10 +1,22 @@
+use std::fmt::Debug;
+
 use ed25519_dalek::SigningKey;
 use share::data_primitives::auth_key_id::AuthKeyId;
 
-#[derive(Debug, typed_builder::TypedBuilder)]
+#[derive(typed_builder::TypedBuilder)]
 pub struct ClientConfig {
     pub api_base: Box<str>,
 
     pub key_id: AuthKeyId,
     pub(super) key: SigningKey,
+}
+
+impl Debug for ClientConfig {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("ClientConfig")
+            .field("api_base", &self.api_base)
+            .field("key_id", &self.key_id)
+            .field("key", &"***")
+            .finish()
+    }
 }
